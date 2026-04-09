86b5902591886be1fa07c56fad36c484f9f5807e
mattybraes <matthewmeiklebraes@gmail.com>
2026-04-09 22:41:02 +0200

    added multiply button and set on click listener

--- app/src/main/java/com/example/multtable/MainActivity.kt
+++ app/src/main/java/com/example/multtable/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.multtable
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val multButton = findViewById<Button>(R.id.multButton)
+        multButton.setOnClickListener {
+            // create the explicte intent
+        }
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
